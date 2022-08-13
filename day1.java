89fa04184423075097a5aacabf06796f407a0154
jyotikaisrani <80196580+jyotikaisrani@users.noreply.github.com>
2022-08-13 23:54:07 +0530

    Day 1
    Learnt and revised the basic concepts of Java

---
+++ day1.java
@@ -0,0 +1,59 @@
+//package com.company;
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) {
+        public static void main(Strings[] args){
+            System.out.println("Sum of 3 numbers");
+            Scanner s = new Scanner(System.in);
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int c = s.nextInt();
+            int sum = a+b+c;
+            System.out.println("sum of 3 numbers is" + sum);
+     
+        /*    // designing CBSE Calculator (Convert marks to percentage)-
+          System.out.println("Marks in subject 1");
+          Scanner sc=new Scanner(System.in);
+          Float a= sc.nextFloat();
+
+          System.out.println("Marks in subject 2");
+          Float b1= sc.nextFloat();
+
+          System.out.println("Marks in subject 3");
+          float c1= sc.nextFloat();
+
+          System.out.println("Marks in subject 4");
+          float d1= sc.nextFloat();
+          
+          float e = (a+ b1+c1+d1)/400*100;
+
+          System.out.println(e);*/
+
+
+
+           /* System.out.println("Taking input from user");
+            Scanner sc= new Scanner(System.in);
+            String str=sc.nextLine();
+            System.out.println(str + str);
+            //checking if the number is an integer 
+            //inputting a line*/
+
+
+
+    }
+}
+    /*    System.out.println("Take input from the user");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number 1");
+     //   int a= sc.nextInt();
+         float a =sc.nextFloat();
+        System.out.println("Enter number 2");
+     //   int b= sc.nextInt();
+         float b=sc.nextFloat();
+     //   int sum=a+b;
+        float sum=a+b;
+       
+        System.out.println(sum);
+    }
+}*/
