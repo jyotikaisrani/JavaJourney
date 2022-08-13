34b0e4020223e50ddc62757ad8fb91a6a6fd10b4
jyotikaisrani <80196580+jyotikaisrani@users.noreply.github.com>
2022-08-13 23:56:46 +0530

    Add files via upload

---
+++ pracday1.java
@@ -0,0 +1,58 @@
+
+
+import java.util.Scanner;
+
+public class pracday1 {
+    //q1- sum of 3 numbers
+    public static void main(String[] args){
+    /*System.out.println("Sum of 3 numbers");
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+    int b = s.nextInt();
+    int c = s.nextInt();
+    int sum = a+b+c;
+    System.out.println("sum of 3 numbers is" + sum);*/
+
+    //q2 - CGPA using marks
+
+  /* System.out.println("Convert Marks to CGPA");
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter marks of subject 1");
+    double a = s.nextDouble();
+    System.out.println("Enter marks of subject 2");
+     double b= s.nextDouble();
+     System.out.println("Enter marks of subject 3");
+     double c = s.nextDouble();
+
+     double d = (a+b+c)/300;
+     double e= d*9.8;
+
+     System.out.println(e);
+    System.out.println("Your CGPA is" +b);*/
+
+    //q3- ask a user to enter his name and greet with, "Hello <name>, have a great day."
+  /*  System.out.println("Enter your name");
+    Scanner s= new Scanner(System.in);
+    String a = s.nextLine();
+    System.out.println("Hello " + a + " have a good day");*/
+
+    //q4 - convert km to miles
+
+    /*System.out.println("Enter km");
+    Scanner s = new Scanner(System.in);
+    double a = s.nextDouble();
+
+    double b= a*0.621371;
+    System.out.println(b+" m");*/
+
+
+    // q5- check whether the number is integer or not.
+    System.out.println("Enter integer"); 
+    Scanner s= new Scanner (System.in);
+     boolean b = s.hasNextInt();
+
+     System.out.println(b);
+
+
+}
+}
