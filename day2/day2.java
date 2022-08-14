ee3623139839ff1bd551eb90e5b6378cae16d957
jyotikaisrani <80196580+jyotikaisrani@users.noreply.github.com>
2022-08-14 14:15:53 +0530

    Add files via upload

---
+++ day2/day2.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[]args){
+       Scanner sc= new Scanner(System.in);
+       /*float a= sc.nextFloat();
+       float b = a%3; //gives remainer
+       System.out.println(b);*/
+
+      /*  System.out.println(64>5 && 64>3); //logical operator */
+
+      //int a = 5*8 - 49/7;
+      /* 
+        left --> right
+        =40-7
+        =33
+       */
+      /*int b = 15/3 - 3*7;
+      System.out.println(b);
+      System.out.println(a); */
+
+     //q1
+      /*int x = 5;
+      int y =6;
+      int k = x * y/2;
+      System.out.println(k); */
+
+      //q2= b*b- 4ac/2a
+      /*int b=1;
+      int a=4;
+      int c=5;
+      int q= b*b - (4*a*c)/(2*a);
+      System.out.println(q);*/
+
+      //q3= u*u-v*v
+
+      /*int u=4;
+      int v=5;
+      int h= u*u - (v*v);
+      System.out.println(h);*/
+
+      /*int a= 5;
+      System.out.println(a++);
+      System.out.println(a);
+      System.out.println(++a);*/
+
+      //q1- float a= 7/4*9/2
+      /*float a= 7/4.0f*9/2.0f; 
+      System.out.println(a); */
+
+      //q2 - Write a java program to encrypt a grade by adding 8 to it. 
+      //Decrypt it to show the correct grade.Use comparison operators to
+      // find out whether a given number is greater than the user entered number or not.
+     //Write the following expression in a java program:
+    //  Character grade = sc.next();
+    /*char grade='B';
+      grade = (char)(grade+8);
+      System.out.println(grade);
+
+      //decrypting 
+      grade = (char)(grade-8);
+      System.out.println(grade); */
+      //q3 - given number is grater than the user entered number
+      //int a = sc.nextInt();
+      //System.out.println(a>8);
+      //q4- int x = 7
+     //int a = 7*49/7 + 35/7
+     int a = (7*49/7 + 35/7);
+     System.out.println(a);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ day2/string_day2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class string_day2 {
+    public static void main(String[] args){
+   // String name = new String("Jyotika");
+  /*  String name = "Jyotika" ; 
+   System.out.println("name is   " +  name);*/
+  // int a=8;
+  // float b=5.6465f;
+  // System.out.printf("value of a is %d and value if b is %.2f " a, b);
+  // System.out.format("The value of a is %d and b is %f," a,b);
+
+
+}
+}
